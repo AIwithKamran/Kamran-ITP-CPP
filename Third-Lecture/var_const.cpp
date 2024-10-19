79fa48ddb28d4183d17e6a59776050b7af434d09
Kamran Khan <kamibhaiofficial01@gmail.com>
2024-10-19 11:58:47 +0500

    Add files via upload

---
+++ Third-Lecture/var_const.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num_1 = 10;
+    const int num_2 = 20;
+    num_1 = 30;
+    cout<<"The First No." << num_1 << endl << "The Second No." << num_2 << endl << "The Sum is " << num_1 + num_2 << endl;
+    // Alt + Shift + F to format the code
+    // Alt + Z to wrap the text
+    return 0;
+}
